df4e6c4ff84c87db08f42f5f7538235a559954bf
VeniceN <164753995+VeniceN@users.noreply.github.com>
2025-05-13 17:10:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Устанавливаем явный URL
+builder.WebHost.UseUrls("http://0.0.0.0:5020");
+
+builder.Services.AddControllers();
+
+// Логирование
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+
+// Обработка HTTP-запросов
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
